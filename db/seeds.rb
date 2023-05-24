0d6f30f615e23a2bc4d4c3af6846ddc2c5af67fe
Emilija Freitaga <emilija.freitaga@gmail.com>
2023-05-24 17:00:41 +0100

    Rails Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Spacious Garden Flat Kent',
+  address: '99 Clifton Street London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area.',
+  price_per_night: 100,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light Flat London',
+  address: '1 Artillery Gardens Hoxton',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 95,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Spacious Flat Soho',
+  address: '10 Firth Street Soho',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 185,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Studio flat London',
+  address: '74 High Street Kensington London',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 2
+)
